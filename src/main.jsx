8f9e6e1acbc0226f0a995f47bcd9a9f0d1488550
fixdevops <fixzdeveloper@gmail.com>
2026-02-24 17:21:45 +0700

    fix: guard supabase env, remove console.log, fix firebase config

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-console.log("App is initializing...");
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
